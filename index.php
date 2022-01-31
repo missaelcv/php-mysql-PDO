7c1d800bbd7702ae6ab282261d428ca707662ce3
missaelcv <missaelcv0220@gmail.com>
2022-01-30 22:43:45 -0400

    Conectado a Sql

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
 
 include_once 'conexion.php';
+
+$sql_leer = 'SELECT * FROM colores';
+
+//Crear Variable
+$gsent = $pdo->prepare($sql_leer);
+$gsent->execute();
+
+$resultado = $gsent->fetchAll();
+
+var_dump($resultado);
+
 ?>
