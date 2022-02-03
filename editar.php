14c9062329540132e7a3ae2d70451f5213fe0c65
missaelcv <missaelcv0220@gmail.com>
2022-02-02 23:43:48 -0400

    agregando heard.editarphp

--- editar.php
+++ editar.php
@@ -17,7 +17,9 @@ $descripcion = $_GET['descripcion'];
 include_once 'conexion.php';
 
 $sql_editar = 'UPDATE colores SET color=?, descripcion=? WHERE id=?';
-
   $sentencia_editar = $pdo->prepare($sql_editar);
   $sentencia_editar->execute(array($color,$descripcion,$id));
+
+  header('location:index.php');
 ?>
+
