c7c042c4735a5fa964fc56a1461c403ce7bb45b2
missaelcv <missaelcv0220@gmail.com>
2022-02-04 23:00:49 -0400

    cerrando conexion en index.php

--- index.php
+++ index.php
@@ -19,8 +19,11 @@ $resultado = $gsent->fetchAll();
   //Crear Variable para la sentencia SQL
   $sentencia_agregar = $pdo->prepare($sql_agregar);
   $sentencia_agregar->execute(array($color,$descripcion));
-    
- // header('location:index.php');
+
+  //Cerrar conexion a la base de datos y sentencias
+  $sentencia_agregar = null;
+  $pdo = null;  
+  header('location:index.php');
  }
 
  if($_GET){
@@ -62,7 +65,6 @@ $resultado = $gsent->fetchAll();
     integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
     <title>Hello, world!</title> -->
 
-    
   </head>
   <body>
     
@@ -112,20 +114,22 @@ $resultado = $gsent->fetchAll();
                     value= "<?php echo $resultado_unico['id'] ?>">
                     <button class="btn btn-primary mt-3">Agregar</button> 
                   </form>
+
                   <?php endif ?>
                 </div>
                 </div>
             </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
+  
   </body>
 </html>
+
+<?php
+
+//Cerramos conexion de la base de datos y sentencias
+$pdo = null;
+$gsent = null;
+?>
