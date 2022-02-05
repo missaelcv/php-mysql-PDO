7a7644a454a2627a57f0898a28179fc5394d7f14
missaelcv <missaelcv0220@gmail.com>
2022-02-04 23:00:05 -0400

    cerrando conexion edita.php

--- editar.php
+++ editar.php
@@ -10,6 +10,9 @@ $sql_editar = 'UPDATE colores SET color=?, descripcion=? WHERE id=?';
   $sentencia_editar = $pdo->prepare($sql_editar);
   $sentencia_editar->execute(array($color,$descripcion,$id));
 
+  //Cerramos conexion de la base de datos y sentencias
+  $pdo = null;
+  $sentencia_editar = null;
   header('location:index.php');
 ?>
 
