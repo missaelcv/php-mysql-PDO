3b910bd174de70cf9e32ca42b101fd6613b7c843
missaelcv <missaelcv0220@gmail.com>
2022-02-04 23:00:26 -0400

    cerrando conexion eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -8,5 +8,10 @@ $sql_eliminar = 'DELETE FROM colores WHERE id=?';
 $sentencia_eliminar = $pdo->prepare($sql_eliminar);
 $sentencia_eliminar->execute(array($id));
 
+//Cerramos conexion de la base de datos y sentencias
+$pdo = null;
+$sentencia_eliminar = null;
+
+
 header('location:index.php');
 
